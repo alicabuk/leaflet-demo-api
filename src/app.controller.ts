f1189012716aa1b26c8b712cafec5b2e5fe18df1
Ali <muhalicabuk@gmail.com>
2024-05-03 18:51:46 +0300

    add endpoint for marker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('marker')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('getallmarker')
+  async getAllMarker() {
+    try {
+      const data = await this.appService.readFromJSON();
+      if (data === null) {
+        return { success: false, message: 'JSON file does not exist.' };
+      }
+      return { success: true, data };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  }
+
+  @Post('addmarker')
+  async addData(@Body() dataItem: any) {
+    try {
+      await this.appService.addData(dataItem);
+      return { success: true, message: 'Data added.' };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  }
+
+  @Post('deletemarker')
+  async deleteData(@Body() data: any) {
+    try {
+      await this.appService.deleteData(data.id);
+      return { success: true, message: 'Data deleted.' };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly filename = 'data.json';
+
+  async readFromJSON() {
+    try {
+      if (!fs.existsSync(this.filename)) {
+        return [];
+      }
+      const jsonData = fs.readFileSync(this.filename, 'utf-8');
+      return JSON.parse(jsonData);
+    } catch (error) {
+      throw new Error(`Error reading from JSON file: ${error.message}`);
+    }
+  }
+
+  async writeToJSON(data: any): Promise<void> {
+    try {
+      for (let index = 0; index < data.length; index++) {
+        if (data.id) {
+          data[index].id = index + 1;
+        }
+      }
+      const jsonData = JSON.stringify(data);
+      fs.writeFileSync(this.filename, jsonData);
+    } catch (error) {
+      throw new Error(`Error writing to JSON file: ${error.message}`);
+    }
+  }
+
+  async addData(dataItem: any): Promise<void> {
+    const dataArray = await this.readFromJSON();
+    const lastId =
+      dataArray.length > 0 ? dataArray[dataArray.length - 1].id : 0;
+    dataItem.id = lastId + 1;
+    dataArray.push(dataItem);
+    await this.writeToJSON(dataArray);
+  }
+
+  async deleteData(id: number): Promise<void> {
+    const dataArray = await this.readFromJSON();
+    const newDataArray = dataArray.filter((item) => item.id !== id);
+    await this.writeToJSON(newDataArray);
   }
 }
